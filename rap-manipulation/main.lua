a256f64bbddcd632e7eab5a87f0c5c0950613409
Ganesh Kumar K <iamtheblackbaron@gmail.com>
2024-02-17 15:18:18 +0530

    Updated rap-manipulation main.lua

--- rap-manipulation/main.lua
+++ rap-manipulation/main.lua
@@ -1,28 +1,40 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Black-o-Baron/luautils/main/printTable.lua"))()
-
 game.Players.LocalPlayer.PlayerScripts.Scripts.Core["Idle Tracking"].Enabled = false
+
+if not config then
+    os.exit()
+end
 
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Library = require(ReplicatedStorage:WaitForChild('Library'))
 local Booths_Broadcast = require(ReplicatedStorage.Network:WaitForChild("Booths_Broadcast"))
 local PlayerData = ""
-local HugeData = {}
 local signal
 
-local function listHuge(hname)
-    print("listHuge(): hname: " .. tostring(hname))
-    if hname then
-        local listingStatus = ReplicatedStorage.Network.Booths_CreateListing:InvokeServer(HugeData[hname][1], HugeData[hname][2], 1)
-        print("listHuge(): listingStatus: " .. tostring(listingStatus))
-    else
-        print("listHuge(): listingStatus: false -> improper args")
+local function listHuge(pos)
+    print("listHuge(): pos: " .. tostring(pos))
+    if not pos then pos = 1 end
+    local Pet = Library.Save.Get().Inventory.Pet
+    local hcount = #config["huges"]
+    local hpos
+    for i = pos, hcount+pos-1, 1 do
+        hpos = (i%hcount)+1
+        for petId, petData in pairs(Pet) do
+            if string.find(petData["id"], "Huge") and string.find(petData["id"], config["huges"][hpos]["name"]) then
+                print("listHuge(): hpos: " .. tostring(hpos) .. " | config: " .. tostring(config["huges"][hpos]["pt"]) .. ", " .. tostring(config["huges"][hpos]["sh"]) .. " | petData: " .. tostring(petData["pt"]) .. ", " .. tostring(petData["sh"]))
+                if ((not config["huges"][hpos]["pt"] and not petData["pt"]) or (config["huges"][hpos]["pt"] and petData["pt"] and config["huges"][hpos]["pt"] == petData["pt"])) and ((not config["huges"][hpos]["sh"] and not petData["sh"]) or (config["huges"][hpos]["sh"] and petData["sh"] and config["huges"][hpos]["sh"] == petData["sh"])) then
+                    local listingStatus = ReplicatedStorage.Network.Booths_CreateListing:InvokeServer(petId, config["huges"][hpos]["price"], 1)
+                    print("listHuge(): listingStatus: " .. tostring(listingStatus))
+                    return listingStatus
+                end
+            end
+        end
     end
 end
 
-local function tryPurchase(uid, playerid, buytimestamp, hname)
-    print("tryPurchase(): uid: " .. tostring(uid) .. " | playerid: " .. tostring(playerid) .. " | buytimestamp: " .. tostring(buytimestamp) .. " | hname: " .. tostring(hname))
-    if uid and playerid and buytimestamp and hname then
+local function tryPurchase(uid, playerid, buytimestamp, pos)
+    print("tryPurchase(): uid: " .. tostring(uid) .. " | playerid: " .. tostring(playerid) .. " | buytimestamp: " .. tostring(buytimestamp) .. " | pos: " .. tostring(pos))
+    if uid and playerid and buytimestamp and pos then
         signal = game:GetService("RunService").Heartbeat:Connect(function()
             if buytimestamp < workspace:GetServerTimeNow() then
                 signal:Disconnect()
@@ -33,27 +45,17 @@ local function tryPurchase(uid, playerid, buytimestamp, hname)
         local purchaseStatus, purchaseMessage = ReplicatedStorage.Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
         print("tryPurchase(): purchaseStatus: " .. tostring(purchaseStatus))
         if purchaseStatus or not config["misc"]["stopOnPurchaseFail"] then
-            listHuge(hname)
+            listHuge(pos)
         end
     else
         print("tryPurchase(): purchaseStatus: false -> improper args")
         if not config["misc"]["stopOnPurchaseFail"] then
-            listHuge(hname)
+            listHuge(pos)
         end
     end
 end
 
 local function init()
-    local Pet = Library.Save.Get().Inventory.Pet
-
-    -- Initialize start in config
-    print("init(): Initialize start in config...")
-    config["start"] = {
-        ["player"] = config["players"][1],
-        ["huge"] = config["huges"][1]["name"]
-    }
-    print("init(): start.player: " .. config["start"]["player"] .. " | start.huge: " .. config["start"]["huge"])
-
     -- Initialize PlayerData
     print("init(): Initialize PlayerData...")
     for i = 1, #config["players"], 1 do
@@ -61,37 +63,18 @@ local function init()
     end
     print("init(): PlayerData: " .. PlayerData)
 
-    -- Initialize HugeData
-    print("init(): Initialize HugeData...")
-    for i = 1, #config["huges"], 1 do
-        for petId, petData in pairs(Pet) do
-            if string.find(petData["id"], "Huge") and string.find(petData["id"], config["huges"][i]["name"]) then
-                print("init(): i: " .. tostring(i) .. " | petData[\"id\"]: " .. petData["id"] .. " | config: " .. tostring(config["huges"][i]["pt"]) .. ", " .. tostring(config["huges"][i]["sh"]) .. " | petData: " .. tostring(petData["pt"]) .. ", " .. tostring(petData["sh"]))
-                if ((not config["huges"][i]["pt"] and not petData["pt"]) or (config["huges"][i]["pt"] and petData["pt"] and config["huges"][i]["pt"] == petData["pt"])) and ((not config["huges"][i]["sh"] and not petData["sh"]) or (config["huges"][i]["sh"] and petData["sh"] and config["huges"][i]["sh"] == petData["sh"])) then
-                    HugeData[config["huges"][i]["name"]] = {petId, config["huges"][i]["price"], config["huges"][(i%3)+1]["name"]}
-                end
-            end
-        end
-    end
-    print("init(): HugeData:")
-    printTable(HugeData, "HugeData")
-
     -- Check for starting player, if true, add the first huge
     print("init(): Initialize add first huge...")
-    if game.Players.LocalPlayer.Name == config["start"]["player"] then
+    if game.Players.LocalPlayer.Name == config["players"][1] then
         print("init(): start player detected. adding first huge...")
-        listHuge(config["start"]["huge"])
+        listHuge()
     end
-
-    task.wait(2)
 
     print("init(): Initialize completed...")
     return true
 end
 
 repeat task.wait() until init()
-
-print("--> BOOTH SNIPER READY <--")
 
 Booths_Broadcast.OnClientEvent:Connect(function(username, message)
     if type(message) == "table" then
@@ -118,12 +101,12 @@ Booths_Broadcast.OnClientEvent:Connect(function(username, message)
             local playerid = message['PlayerID']
             local unitGems = gems / amount
 
-            print("Booths_Broadcast: listing: " .. tostring(item) .. " | " .. tostring(playerid) .. " | " .. tostring(uid))
+            print("Booths_Broadcast: listing: " .. tostring(playerid) .. " | " .. tostring(uid) .. " | " .. tostring(item) .. " | " .. tostring(unitGems))
 
-            for hname, hvalues in pairs(HugeData) do
-                if string.find(PlayerData, tostring(playerid)) and string.find(item, hname) and unitGems == hvalues[2] then
+            for i = 1, #config["huges"], 1 do
+                if string.find(PlayerData, tostring(playerid)) and string.find(item, config["huges"][i]["name"]) and unitGems == config["huges"][i]["price"] then
                     print("Booths_Broadcast: match found. calling tryPurchase...")
-                    coroutine.wrap(tryPurchase)(uid, playerid, buytimestamp, hvalues[3])
+                    coroutine.wrap(tryPurchase)(uid, playerid, buytimestamp, i)
                 end
             end
         end
