df4b9dcb7812e1ef7dc01a9eccb66e882cd841d4
Ganesh Kumar K <iamtheblackbaron@gmail.com>
2024-02-28 21:02:30 +0530

    modified serverhop to check 10 servers at a time (attempted to randomise servers visited as much as possible)

--- farming/auto-big-chests-with-serverhop.lua
+++ farming/auto-big-chests-with-serverhop.lua
@@ -242,12 +242,12 @@ if SERVER_HOP then
     task.wait(SERVER_HOP_DELAY)
 
     local sfUrl = "https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=%s&limit=%s&excludeFullGames=true" 
-    local req = request({ Url = string.format(sfUrl, 8737899170, "Asc", 50) }) 
+    local req = request({ Url = string.format(sfUrl, 8737899170, "Asc", 10) }) 
     local body = game:GetService("HttpService"):JSONDecode(req.Body) 
-    local deep = math.random(1, 3)
+    local deep = math.random(1, 10)
     if deep > 1 then
         for i = 1, deep, 1 do
-            req = request({ Url = string.format(sfUrl .. "&cursor=" .. body.nextPageCursor, 8737899170, "Asc", 50) }) 
+            req = request({ Url = string.format(sfUrl .. "&cursor=" .. body.nextPageCursor, 8737899170, "Asc", 10) }) 
             body = game:GetService("HttpService"):JSONDecode(req.Body) 
             task.wait(0.1)
         end
