0e8bc5f64e5a34f63158aac0391d7add9cc3f8d7
Ganesh Kumar Kannan <iamtheblackbaron@gmail.com>
2024-03-16 10:04:02 +0530

    Added reference.lua

---
+++ booth-sniper/reference.lua
@@ -0,0 +1,192 @@
+--[[ ======================================= POTIONS ======================================= ]]
+local args = {
+    [1] = "Potion",
+    [2] = "{\"id\":\"Treasure Hunter\",\"tn\":9}",
+    [4] = false
+}
+game:GetService("ReplicatedStorage").Network.TradingTerminal_Search:InvokeServer(unpack(args))
+local args = {
+    [1] = "Potion",
+    [2] = "{\"id\":\"The Cocktail\",\"tn\":1}",
+    [4] = false
+}
+
+game:GetService("ReplicatedStorage").Network.TradingTerminal_Search:InvokeServer(unpack(args))
+local args = {
+    [1] = "Potion",
+    [2] = "{\"id\":\"Huge\",\"tn\":1}",
+    [4] = false
+}
+game:GetService("ReplicatedStorage").Network.TradingTerminal_Search:InvokeServer(unpack(args))
+local args = {
+    [1] = "Potion",
+    [2] = "{\"id\":\"Diamonds\",\"tn\":7}",
+    [4] = false
+}
+game:GetService("ReplicatedStorage").Network.TradingTerminal_Search:InvokeServer(unpack(args))
+local args = {
+    [1] = "Potion",
+    [2] = "{\"id\":\"Walkspeed\",\"tn\":3}",
+    [4] = false
+}
+game:GetService("ReplicatedStorage").Network.TradingTerminal_Search:InvokeServer(unpack(args))
+
+--[[ ======================================= ITEMS ======================================= ]]
+
+local args = {
+    [1] = "Misc",
+    [2] = "{\"id\":\"Secret Key Upper Half\"}",
+    [4] = false
+}
+game:GetService("ReplicatedStorage").Network.TradingTerminal_Search:InvokeServer(unpack(args))
+local args = {
+    [1] = "Fruit",
+    [2] = "{\"id\":\"Candycane\"}",
+    [4] = false
+}
+game:GetService("ReplicatedStorage").Network.TradingTerminal_Search:InvokeServer(unpack(args))
+local args = {
+    [1] = "Seed",
+    [2] = "{\"id\":\"Diamond\"}",
+    [4] = false
+}
+game:GetService("ReplicatedStorage").Network.TradingTerminal_Search:InvokeServer(unpack(args))
+local args = {
+    [1] = "Misc",
+    [2] = "{\"id\":\"Large Gift Bag\"}",
+    [4] = false
+}
+game:GetService("ReplicatedStorage").Network.TradingTerminal_Search:InvokeServer(unpack(args))
+local args = {
+    [1] = "Misc",
+    [2] = "{\"id\":\"Gift Bag\"}",
+    [4] = false
+}
+game:GetService("ReplicatedStorage").Network.TradingTerminal_Search:InvokeServer(unpack(args))
+local args = {
+    [1] = "Misc",
+    [2] = "{\"id\":\"Insta Plant Capsule\"}",
+    [4] = false
+}
+game:GetService("ReplicatedStorage").Network.TradingTerminal_Search:InvokeServer(unpack(args))
+local args = {
+    [1] = "Misc",
+    [2] = "{\"id\":\"Breakable Sprinkler\"}",
+    [4] = false
+}
+game:GetService("ReplicatedStorage").Network.TradingTerminal_Search:InvokeServer(unpack(args))
+local args = {
+    [1] = "Misc",
+    [2] = "{\"id\":\"Charm Hammer\"}",
+    [4] = false
+}
+game:GetService("ReplicatedStorage").Network.TradingTerminal_Search:InvokeServer(unpack(args))
+local args = {
+    [1] = "Misc",
+    [2] = "{\"id\":\"Charm Chisel\"}",
+    [4] = false
+}
+game:GetService("ReplicatedStorage").Network.TradingTerminal_Search:InvokeServer(unpack(args))
+local args = {
+    [1] = "Misc",
+    [2] = "{\"id\":\"Exotic Treasure Flag\"}",
+    [4] = false
+}
+game:GetService("ReplicatedStorage").Network.TradingTerminal_Search:InvokeServer(unpack(args))
+local args = {
+    [1] = "Misc",
+    [2] = "{\"id\":\"Party Box\"}",
+    [4] = false
+}
+game:GetService("ReplicatedStorage").Network.TradingTerminal_Search:InvokeServer(unpack(args))
+
+--[[ ======================================= ENCHANTS ======================================= ]]
+
+local args = {
+    [1] = "Enchant",
+    [2] = "{\"id\":\"Boss Chest Mimic\",\"tn\":1}",
+    [4] = false
+}
+game:GetService("ReplicatedStorage").Network.TradingTerminal_Search:InvokeServer(unpack(args))
+local args = {
+    [1] = "Enchant",
+    [2] = "{\"id\":\"Chest Mimic\",\"tn\":1}",
+    [4] = false
+}
+game:GetService("ReplicatedStorage").Network.TradingTerminal_Search:InvokeServer(unpack(args))
+local args = {
+    [1] = "Enchant",
+    [2] = "{\"id\":\"Diamond Chest Mimic\",\"tn\":1}",
+    [4] = false
+}
+game:GetService("ReplicatedStorage").Network.TradingTerminal_Search:InvokeServer(unpack(args))
+local args = {
+    [1] = "Enchant",
+    [2] = "{\"id\":\"Lucky Block\",\"tn\":1}",
+    [4] = false
+}
+game:GetService("ReplicatedStorage").Network.TradingTerminal_Search:InvokeServer(unpack(args))
+local args = {
+    [1] = "Enchant",
+    [2] = "{\"id\":\"Massive Comet\",\"tn\":1}",
+    [4] = false
+}
+game:GetService("ReplicatedStorage").Network.TradingTerminal_Search:InvokeServer(unpack(args))
+local args = {
+    [1] = "Enchant",
+    [2] = "{\"id\":\"Super Shiny Hunter\",\"tn\":1}",
+    [4] = false
+}
+game:GetService("ReplicatedStorage").Network.TradingTerminal_Search:InvokeServer(unpack(args))
+local args = {
+    [1] = "Enchant",
+    [2] = "{\"id\":\"Strong Pets\",\"tn\":2}",
+    [4] = false
+}
+game:GetService("ReplicatedStorage").Network.TradingTerminal_Search:InvokeServer(unpack(args))
+local args = {
+    [1] = "Enchant",
+    [2] = "{\"id\":\"Walkspeed\",\"tn\":2}",
+    [4] = false
+}
+game:GetService("ReplicatedStorage").Network.TradingTerminal_Search:InvokeServer(unpack(args))
+
+--[[ ======================================= CHARMS ======================================= ]]
+
+local args = {
+    [1] = "Charm",
+    [2] = "{\"id\":\"Royalty\",\"tn\":1}",
+    [4] = false
+}
+game:GetService("ReplicatedStorage").Network.TradingTerminal_Search:InvokeServer(unpack(args))
+local args = {
+    [1] = "Charm",
+    [2] = "{\"id\":\"Glittering\",\"tn\":1}",
+    [4] = false
+}
+game:GetService("ReplicatedStorage").Network.TradingTerminal_Search:InvokeServer(unpack(args))
+local args = {
+    [1] = "Charm",
+    [2] = "{\"id\":\"Overload\",\"tn\":1}",
+    [4] = false
+}
+game:GetService("ReplicatedStorage").Network.TradingTerminal_Search:InvokeServer(unpack(args))
+
+--[[ ======================================= PETS ======================================= ]]
+
+-- remove pt and sh key for normal non-shiny
+-- pt = 1 for gold and 2 for rainbow
+-- sh = true for shiny
+
+local args = {
+    [1] = "Pet",
+    [2] = "{\"id\":\"Huge Amethyst Dragon\",\"pt\":2,\"sh\":true}",
+    [4] = false
+}
+game:GetService("ReplicatedStorage").Network.TradingTerminal_Search:InvokeServer(unpack(args))
+local args = {
+    [1] = "Pet",
+    [2] = "{\"id\":\"Amethyst Dragon\",\"pt\":2,\"sh\":true}",
+    [4] = false
+}
+game:GetService("ReplicatedStorage").Network.TradingTerminal_Search:InvokeServer(unpack(args))
