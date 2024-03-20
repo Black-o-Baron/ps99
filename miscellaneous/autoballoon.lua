7e76edbf2e05592dcf856ff69a4019b0770142e6
Ganesh Kumar K <53404348+Black-o-Baron@users.noreply.github.com>
2024-03-20 17:34:17 +0530

    Update autoballoon.lua

--- miscellaneous/autoballoon.lua
+++ miscellaneous/autoballoon.lua
@@ -8,7 +8,7 @@ getgenv().MoneyPrinter = {
 
     serverHopper = true,
     avoidCooldown = false,
-    minServerTime = 10, -- Avoid 268 if multi-accounting : Force stay in server for x time even if no Balloons
+    minServerTime = 20, -- Avoid 268 if multi-accounting : Force stay in server for x time even if no Balloons
 
     sendWeb = true,
     webURL = "https://discord.com/api/webhooks/1219484976781721723/8dNBZ8UsnmA7G1E3iMS-nUDy3qYMKQ_A1m1DnNHsHWVTNIvh56mqwWjEZq3X5koejyGY",
