d04ed43c060634b506e2c3e9ab7ab5c5f0dd55f4
Ganesh Kumar K <53404348+Black-o-Baron@users.noreply.github.com>
2024-03-26 00:19:50 +0530

    Update auto-gardening.lua

--- gardening/auto-gardening.lua
+++ gardening/auto-gardening.lua
@@ -5,17 +5,39 @@ getgenv().Water = true
 local Player = game:GetService("Players").LocalPlayer
 local RepStor = game.ReplicatedStorage
 local Library = require(RepStor.Library)
+local HRP = Player.Character.HumanoidRootPart
 local saveMod = require(RepStor.Library.Client.Save)
+
+local args
 
 function getDiamonds() return Player.leaderstats["💎 Diamonds"].Value end
 
 function getInfo(name) return saveMod.Get()[name] end
 
-local lootbags = nil
-lootbags = workspace.__THINGS.Lootbags.ChildAdded:Connect(function(v)
-    Library.Network.Fire("Lootbags_Claim", { v.Name })
-    task.wait()
-    v:Destroy()
+-- AUTO ORB
+local autoOrbConnection = nil
+local autoLootBagConnection = nil
+for i, v in workspace.__THINGS.Orbs:GetChildren() do
+	Library.Network.Fire("Orbs: Collect",{tonumber(v.Name)})
+	Library.Network.Fire("Orbs_ClaimMultiple",{[1]={[1]=v.Name}})
+	task.wait()
+	v:Destroy()
+end
+for i, v in workspace.__THINGS.Lootbags:GetChildren() do
+	Library.Network.Fire("Lootbags_Claim",{v.Name})
+	task.wait()
+	v:Destroy()
+end
+autoOrbConnection = workspace.__THINGS.Orbs.ChildAdded:Connect(function(v)
+	Library.Network.Fire("Orbs: Collect",{tonumber(v.Name)})
+	Library.Network.Fire("Orbs_ClaimMultiple",{[1]={[1]=v.Name}})
+	task.wait()
+	v:Destroy()
+end)
+autoLootBagConnection = workspace.__THINGS.Lootbags.ChildAdded:Connect(function(v)
+	Library.Network.Fire("Lootbags_Claim",{v.Name})
+	task.wait()
+	v:Destroy()
 end)
 
 pcall(function()
@@ -59,13 +81,37 @@ if getDiamonds() >= 30000 then
 end
 
 while getgenv().autoGarden do
+    HRP.CFrame = CFrame.new(184,23,1989) -- TP to gardening entry door
+    task.wait(60) -- Added delay here just incase of unexpected lag during teleport
+    HRP.CFrame = CFrame.new(-449,110,-1399) -- TP to center pot
     task.wait()
     for i = 1, 10 do
-        Library.Network.Invoke("Instancing_InvokeCustomFromClient", "FlowerGarden", "PlantSeed", i, "Diamond")
-        if getgenv().InstaPlant then Library.Network.Invoke("Instancing_InvokeCustomFromClient", "FlowerGarden",
-                "InstaGrowSeed", i) end
-        if getgenv().Water then Library.Network.Invoke("Instancing_InvokeCustomFromClient", "FlowerGarden", "WaterSeed",
-                i) end
-        Library.Network.Invoke("Instancing_InvokeCustomFromClient", "FlowerGarden", "ClaimPlant", i)
+        Library.Network.Invoke("Instancing_InvokeCustomFromClient", "FlowerGarden", "PlantSeed", i, "Diamond") -- Plant seed
+        if getgenv().InstaPlant then Library.Network.Invoke("Instancing_InvokeCustomFromClient", "FlowerGarden", "InstaGrowSeed", i) end
+        if getgenv().Water then Library.Network.Invoke("Instancing_InvokeCustomFromClient", "FlowerGarden", "WaterSeed", i) end -- Water seed
+        Library.Network.Invoke("Instancing_InvokeCustomFromClient", "FlowerGarden", "ClaimPlant", i) -- Claim plant
     end
+    task.wait()
+    HRP.CFrame = CFrame.new(-533,108,-1401) -- TP to gardening exit door
+    task.wait(60) -- Added delay here just incase of unexpected lag during teleport
+    HRP.CFrame = CFrame.new(819,16,1493) -- TP to Advanced Merchant area center point
+    for i = 1, 6 do
+        task.wait()
+        args = {
+            [1] = "AdvancedMerchant",
+            [2] = i
+        }
+        game:GetService("ReplicatedStorage").Network.Merchant_RequestPurchase:InvokeServer(unpack(args))
+    end
+    task.wait(60)
+    HRP.CFrame = CFrame.new(260,16,2145) -- TP to Garden Merchant area center point
+    for i = 1, 6 do
+        task.wait()
+        args = {
+            [1] = "GardenMerchant",
+            [2] = i
+        }
+        game:GetService("ReplicatedStorage").Network.Merchant_RequestPurchase:InvokeServer(unpack(args))
+    end
+    task.wait(1300)
 end
